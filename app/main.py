8bf447b6bbac8e2d66e4131a57c4ce5b409692b0
Hayden <44925247+Koalacards@users.noreply.github.com>
2025-10-05 08:12:24 -0400

    do the thing

--- app/main.py
+++ app/main.py
@@ -10,12 +10,4 @@ def on_startup() -> None:
     create_db_and_tables()
     seed_database()
 
-
-@app.get("/health")
-def root_health() -> dict:
-    return {"status": "ok"}
-
-
 app.include_router(api_router)
-
-
