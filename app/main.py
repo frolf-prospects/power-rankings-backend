62af79d09727ae84048e156928fdd0ae4f195c72
Hayden <44925247+Koalacards@users.noreply.github.com>
2025-10-12 00:04:54 -0400

    update cors middleware for testing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .db import create_db_and_tables, seed_database
 from .api.routes import api_router
 
 app = FastAPI(title="Power Rankings Backend")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup() -> None:
